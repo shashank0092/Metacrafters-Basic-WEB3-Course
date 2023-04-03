eab4b5370fe6825e887816bdf5d479fdf79d5345
SHASHANK <shashank7016204503@gmail.com>
2023-04-03 12:50:27 +0530

    Completed Storage module

---
+++ ETH PROOF Intermediate EVM Course/Solidity/Storage.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+
+contract Stroage {
+
+    // THIS WILL BECOME STORAGE VARIAVBLE
+    uint256 public data=10;
+
+    function boi()public pure  {
+        // THIS WILL BECOME MEMOREY VARAIBLE AND ACVLIABLE UNTILL THE FUNCTION EXUCATION CALL
+        uint ans=10;
+
+    }
+}

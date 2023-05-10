01580596851979f207f55f977922b9695e2ead88
SHASHANK <shashank7016204503@gmail.com>
2023-05-10 10:20:45 +0530

    Completed Advance Course Module3 Challange

---
+++ ETH PROOF Advanced EVM Course/Part3 Project/StorageVictimV8.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract StorageVictim {
+    
+    address public owner;
+    
+    struct Storage {
+        address user;
+        uint256 amount;
+    }
+    
+    mapping(address => Storage) storages;
+
+    constructor() {
+        owner = msg.sender;
+    }
+    
+    function store(uint256 _amount) public {
+        Storage memory str = Storage(msg.sender, _amount);
+        storages[msg.sender] = str;
+    }
+    
+    function getStore() public view returns (address, uint256) {
+        Storage memory str = storages[msg.sender];
+        return (str.user, str.amount);
+    }
+    
+    function getOwner() public view returns (address) {
+        return owner;
+    }
+}
